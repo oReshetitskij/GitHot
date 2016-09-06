912fd2394e7e97b59040667f053d5ff56f06e8cb
Oleksandr Reshetitskij <sanja_reshetitskij@ukr.net>
2016-09-06 19:21:42 +0300

    Moved configuration to config.json Created Configuration class

---
+++ GitHot.Core/Configuration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octokit.Internal;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace GitHot.Core
+{
+    class Configuration
+    {
+        public string Token { get; protected set; }
+        public int MaxSubscriptions { get; protected set; }
+        public int PageCount { get; protected set; }
+        public int ItemsPerPage { get; protected set; }
+
+        private static Configuration _instance;
+
+        public static Configuration Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    var serializer = new SimpleJsonSerializer();
+
+                    string json = File.ReadAllText("config.json");
+
+                    _instance = serializer.Deserialize<Configuration>(json);
+                }
+
+                return _instance;
+            }
+            set { _instance = value; }
+        }
+    }
+}
