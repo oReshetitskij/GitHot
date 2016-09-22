47769c27364b66aff8303953135c2ece3ee3b19d
Oleksandr Reshetitskij <sanja_reshetitskij@ukr.net>
2016-09-22 16:31:53 +0300

    Added POCO classes for serialization

---
+++ GitHot.Core/POCO/TopRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHot.Core.POCO
+{
+    public class TopRepository
+    {
+        public string Criteria { get; set; }
+        public int Value { get; set; }
+        public int Weeks { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string OwnerName { get; set; }
+        public string OwnerUrl { get; set; }
+    }
+}
---
+++ GitHot.Core/POCO/TrendingRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Octokit;
+
+namespace GitHot.Core.POCO
+{
+    class TrendingRepository
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string OwnerName { get; set; }
+        public string OwnerUrl { get; set; }
+        public int[] Stars { get; set; }
+        public int[] Commits { get; set; }
+        public int[] Contributors { get; set; }
+
+        public TimeSpan Span { get; set; }
+    }
+}
