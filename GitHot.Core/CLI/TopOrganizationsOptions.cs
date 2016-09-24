bccb456889355ec2f6e6eb639c04c613fdef5bca
Oleksandr Reshetitskij <sanja_reshetitskij@ukr.net>
2016-09-24 21:39:44 +0300

    Added files not staged at previous commit

---
+++ GitHot.Core/CLI/TopOrganizationsOptions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace GitHot.Core.CLI
+{
+    class TopOrganizationsOptions
+    {
+        [Option('c', "count", Required = true, HelpText = "Number of organizations to show", DefaultValue = 50)]
+        public int Count { get; set; }
+
+        [Option('w', "weeks", Required = true, HelpText = "Number of weeks for collecting statistics", DefaultValue = 1)]
+        public int Weeks { get; set; }
+
+        [Option('o', "output", HelpText = "Filepath for saving output")]
+        public string Output { get; set; }
+
+        [Option('t', "total", MutuallyExclusiveSet = "total", HelpText = "Get top repositories by total commits count")]
+        public bool TotalCommits { get; set; }
+
+        [Option('a', "avg", MutuallyExclusiveSet = "avg", HelpText = "Get top repositories by average commits count")]
+        public bool AverageCommits { get; set; }
+
+        public string GetUsageDescription(string givenName)
+        {
+            return $"githot {givenName} - discover top organizations by certain criteria.\n" +
+                   $"Usage: githot {givenName} -o <path> [-c|--count] [-w|--weeks] [-t|--total|-a|--avg]";
+        }
+    }
+}
---
+++ GitHot.Core/POCO/TopOrganization.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHot.Core.POCO
+{
+    class TopOrganization
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public int? TotalCommits { get; set; }
+        public double? MemberAverageCommits { get; set; }
+        public TimeSpan Span { get; set; }
+    }
+}
